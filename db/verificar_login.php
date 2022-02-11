a484838d8a534a0a2b78c3c813390f9bda7e985b
rodosd79 <rodosd79@hotmail.com>
2022-02-11 20:01:42 -0300

    atualização

--- db/verificar_login.php
+++ db/verificar_login.php
@@ -4,5 +4,27 @@ require_once "conexao.php";
 session_start();
 
 if(isset($_REQUEST['logar'])){
-    $email = $
+    $email = $_POST['email'];
+    $senha = md5($_POST['senha']);
+
+    //consulta ao banco de dados e validação
+
+    $query = "SELECT * FROM usuario WHERE email = '$email' AND senha = '$senha'";
+    $result = mysqli_query($conn, $query);
+
+    //verificação de resultado
+
+    if(mysqli_num_rows($result) > 0){
+
+        $dados = mysqli_fetch_array($result);
+        $_SESSION['id'] = $dados['id'];
+        $_SESSION['nome'] = $dados['nome'];
+        $_SESSION['email'] = $dados['email'];
+
+        header('location:index.php');
+
+    } else {
+        
+        header('location:login.php?erro=1');
+    }
 }
