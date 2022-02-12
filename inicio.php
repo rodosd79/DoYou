f41d1be1beb67ebf3231a8a601ee3e6c6a8dd6ec
Ikaro <ikarovieiraa@gmail.com>
2022-02-12 03:37:46 -0300

    Mostra saldo

---
+++ inicio.php
@@ -0,0 +1,42 @@
+<?php 
+require_once "db/db_inicio.php";
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DoYou | Relatórios</title>
+    <!-- DoYou style -->
+    <link rel="stylesheet" href="assets/css/style-inicio.css">
+    <link rel="stylesheet" href="assets/css/style_header.css">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+</head>
+<body>
+    <!-- Header -->
+        <?php include_once "header.php";
+
+        if (!$_SESSION['nome']){
+            header('location: login.php');
+            exit();
+        }
+        ?>
+    <!--  -->
+    
+    <h4 class="ml-4 pl-1 w-100"> <strong class="DoYou"> Saldo: R$ </strong><?php echo $saldo; ?></h4>
+
+    <div></div>
+
+    <!-- Footer -->
+        <?php include_once "footer.php" ?>
+    <!--  -->
+
+    <!-- Bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+    <!--  -->
+</body>
+</html>
