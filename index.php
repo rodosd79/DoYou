101e29b23d441994920b8bcffcc895dd7e85fb5e
Ikaro <ikarovieiraa@gmail.com>
2022-03-02 20:10:09 -0300

    carrossel

--- index.php
+++ index.php
@@ -13,7 +13,12 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
     <link rel="shortcut icon" href="assets/img/favicon.ico">
-
+    <style>
+        #carouselExampleCaptions {
+            border-radius: 100% !important;
+            color: #00d863;
+        }
+    </style>
 </head>
 <body>
     <!-- CABEÇALHO -->
@@ -21,7 +26,42 @@
     <!--  -->
 
     <!-- CARROSSEL -->
-        <img src="assets/img/ft1.png" class="img-fluid" alt="">
+
+        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <?php require_once "db/db_index.php";$cont = 0;
+                foreach ($carrossel as $c){ ?>
+                <li data-target="#carouselExampleCaptions" data-slide-to="$cont" class="active"></li>
+                <?php $cont++; } ?>
+            </ol>
+       
+            <div class="carousel-inner">
+                <?php
+                $contador = 0;
+                foreach($carrossel as $c){ 
+                $contador ++;
+                if($contador == 1) { ?>
+                    <div class="carousel-item active">
+                <?php } else { ?>
+                    <div class="carousel-item">
+                <?php } ?>
+                        <img src="<?php echo $c['imagem'] ?>" class="d-block w-100" style="height: 550px;" alt="<?php echo $c['alt_img'] ?>">
+                        <div class="carousel-caption d-none  d-md-block">
+                            <h5 style="color: white; background-color: #00d863; font-size: 20pt; border-radius: 2rem;" class="w-100 py-1 px-2"><?php echo $c['titulo'] ?> </h5>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        
+            <button class="carousel-control-prev" type="button" data-target="#carouselExampleCaptions" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-target="#carouselExampleCaptions" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </button>
+        </div>
     <!--  -->
 
     <h3 class="w-100 my-5 py-1 h3DoYou text-center">Sobre nós</h3>
